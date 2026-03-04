210e3d210804c38cd6b82d1032215b4523562d29
adamgroenhout <adamgroenhout@users.noreply.github.com>
2026-03-03 22:18:13 -0500

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://adamgroenhout.github.io",
+  base: "/wildwood-site/",
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
